aed38eb10707ad5e18f7c39a085c5b59d105dd27
Graf-J <johannes.graf.1908@gmail.com>
2024-03-09 12:26:49 +0100

    Add Loading Screen for Graph-Editor

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useState, useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
+import { Spinner, Skeleton } from '@nextui-org/react';
 import GraphBuilder from '@/components/custom/graph-builder';
 import Sidebar from './sidebar';
 import Toolbar from './toolbar';
@@ -20,27 +21,37 @@ export default function Page({ params }: { params: { id: string } }) {
                 if (response.type === HttpResponseType.GENERAL_ERROR) {
                     toast.error(response.generalErrorMessage);
                 } else {
-                    setGraph(Graph.fromResponse(response.response!))
+                    setGraph(Graph.fromResponse(response.response!));
                 }
             } catch {
-                toast.error('Internal Server Error')
+                toast.error('Internal Server Error');
             }
         }
 
-        fetchGraph()
+        fetchGraph();
     }, [params.id, setGraph])
 
   return (
     <div className="p-3 flex h-screen bg-gray-700 dark:bg-gray-500">
         <div className="bg-gray-200 dark:bg-gray-800 border-r-2 border-gray-700 dark:border-gray-300 flex rounded-tl-lg rounded-bl-lg" style={{ width: 500 }}>
             <div className="flex-1 p-2">
-                { graph && <Sidebar projectId={params.id} /> }
+                { graph ? 
+                    <Sidebar projectId={params.id} /> 
+                : 
+                    <div className="h-full flex items-center justify-center">
+                        <Spinner size="lg" />
+                    </div> 
+                }
             </div>
         </div>
         <div className="flex-1 flex flex-col rounded-tr-lg rounded-br-lg">
             <Toolbar projectId={params.id} />
             <div className="shadow-lg h-full w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-br-lg">
-                { graph && <GraphBuilder projectId={params.id} /> }
+                { graph ? 
+                    <GraphBuilder projectId={params.id} /> 
+                :
+                    <Skeleton className="dark:dark rounded-lg h-full w-full" />
+                }
             </div>
         </div>
     </div>
